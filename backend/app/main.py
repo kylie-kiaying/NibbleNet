683be785ca8addc686352085676b96ad8198fe72
kylie-kiaying <kylie.kiayingchua@gmail.com>
2024-06-16 17:18:05 +0800

    Added router in main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from .routers.user_routers import router as user_router
 
 app = FastAPI()
 
+app.include_router(user_router, prefix="/users", tags=["users"])
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+  return {"message": "Hello World"}
